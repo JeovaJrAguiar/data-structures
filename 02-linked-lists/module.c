a527b658319cd9213a49b97ca004cde8d8c170d2
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2023-01-21 18:47:14 -0300

    .

--- 02-linked-lists/module.c
+++ 02-linked-lists/module.c
@@ -4,3 +4,35 @@
 // Linked List
 
 #include "lib.h"
+
+NO* createNode(int value){
+	NO* no = (NO*) malloc(sizeof(NO*));
+	no->prev = null;
+	no->next = null;
+	no->value = value;
+}
+
+// return "O" if Ok
+// return "N" if NO-OK
+char insertNode(NO* root, NO* element){
+	if(root == null || element == null) return "N";
+	
+	NO* noAux = createNode();
+	noAux = root;
+	int aux = element.value;
+	char opt = "I";
+
+	do{
+                if(noAux.value > element.value) opt = "O";
+		noAux = noAux->next;
+	}while(opt != "O")
+	
+	element->next = &noAux;
+	element->prev = &noAux->prev;
+	noAux->prev->next = element;
+	noAux->prev = element;
+
+	return "O"
+}
+
+char removeNode(NO* root, int element);
